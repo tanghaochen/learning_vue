4f816b8436b8edeba45ade0b47a71c7bcd3a88ee
唐昊辰 <1774189597@qq.com>
2022-03-08 21:25:04 +0800

    index ui component

--- ui-create1/src/components/common/card-index.vue
+++ ui-create1/src/components/common/card-index.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="py-4 bg-secondary text-light  bg-opacity-50 rounded-3 flex-grow-1 m-1 d-flex flex-column text-center justify-content-center">
-    <my-icon class="icon-kong text-danger my-3" size="60"></my-icon>
-    <span>About me</span>
+  <div
+    class="py-4 bg-secondary text-light bg-opacity-50 rounded-3 flex-grow-1 m-1 d-flex flex-column text-center justify-content-center"
+  >
+    <my-icon class="icon-kong text-danger my-3" :size="size"></my-icon>
+    <span>
+      <slot></slot>
+    </span>
   </div>
 </template>
 
 <script>
 export default {
   name: "card-index",
-  components: {}
-
-}
+  components: {},
+  props: {
+    id: {
+      type: String,
+      default: "",
+    },
+    color: {
+      type: String,
+      default: "",
+    },
+    size: {
+      type: [Number, String],
+      default: 40,
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- ui-create1/src/views/HomeView.vue
+++ ui-create1/src/views/HomeView.vue
@@ -4,24 +4,34 @@
       <!--left-->
       <slide-bar />
       <!--      center-->
-      <div style="display: flex;flex: 1" class="p-1 d-flex align-content-center justify-content-center flex-column">
+      <div
+        style="display: flex; flex: 1"
+        class="p-1 d-flex align-content-center justify-content-center flex-column"
+      >
         <div class="text-center text-white my-4">
-          <div style="font-size:3rem">I'M <span style="color: #02eefa;">Mark</span></div>
+          <div style="font-size: 3rem">
+            I'M <span style="color: #02eefa">Mark</span>
+          </div>
           <div>您已被点赞300次</div>
         </div>
-        <div class="flex-fill d-flex flex-column pb-5">
+        <div class="flex-fill d-flex flex-column pb-5 py">
           <div class="d-flex" style="flex: 1">
-            <card-index></card-index>
-            <card-index></card-index>
+            <card-index size="90">About me</card-index>
+            <card-index>Resume</card-index>
           </div>
           <div class="d-flex" style="flex: 1">
-            <card-index></card-index>
+            <card-index>Portfolio</card-index>
           </div>
         </div>
       </div>
       <!--      right-->
       <!--        <image src="https://picsum.photos/id/11/10/6" alt="1" class="flex-fill bg-primary h-100 w-100" style="width:100px;height:100px"></image>-->
-      <img src="../assets/img/wr-user.jpg" alt="用户画像" style="flex-grow: 0;width:30vw" class="img-thumbnail"/>
+      <img
+        src="../assets/img/wr-user.jpg"
+        alt="用户画像"
+        style="flex-grow: 0; width: 30vw"
+        class="img-thumbnail px"
+      />
     </div>
   </div>
 </template>
@@ -34,12 +44,11 @@ export default {
   name: "Home",
   components: {
     cardIndex,
-    slideBar
-    ,
+    slideBar,
   },
   setup() {
     return {};
-  }
+  },
 };
 </script>
 
@@ -50,7 +59,6 @@ export default {
   justify-content: center;
   align-items: center;
   height: 100vh;
-
 }
 
 #home {
@@ -63,6 +71,4 @@ export default {
 .left-fonts {
   writing-mode: vertical-rl;
 }
-
-
 </style>
